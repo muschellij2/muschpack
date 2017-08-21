0a4cd0dc545e296f68092f83c41925fe51a30763
muschellij2 <muschellij2@gmail.com>
2017-08-21 15:57:10 -0400

    new readme.

--- tests/testthat/test-r_package_repos.R
+++ tests/testthat/test-r_package_repos.R
@@ -1,4 +1,6 @@
 test_that("r_package_repos", {
-  testthat::expect_success(expr = r_package_repos(username = "muschellij2"))
-
+  testthat::expect_success(
+    expr = {
+      r_package_repos(username = "muschellij2")
+      })
 })
