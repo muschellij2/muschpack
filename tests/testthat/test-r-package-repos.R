b3960134f4484d64bd03a3f089e0729ea63ca87c
muschellij2 <muschellij2@gmail.com>
2017-08-21 16:12:42 -0400

    Fixing the tests.

---
+++ tests/testthat/test-r-package-repos.R
@@ -0,0 +1,3 @@
+test_that("r_package_repos", {
+  testthat::expect_silent(r_package_repos(username = "muschellij2"))
+})
--- tests/testthat/test-r_package_repos.R
+++
@@ -1,6 +0,0 @@
-test_that("r_package_repos", {
-  testthat::expect_success(
-    expr = {
-      r_package_repos(username = "muschellij2")
-      })
-})
