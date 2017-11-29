7f3a4c629cd7c3a7a3665771b1c18f2e126b56db
muschellij2 <muschellij2@gmail.com>
2017-11-29 18:44:22 -0500

    Added package_table for neuroc in the readme too.

---
+++ R/pack_table.R
@@ -0,0 +1,33 @@
+#' Package Table for Printing
+#'
+#' @param df A \code{data.frame} from \code{r_package_repos} that has
+#' been subset to what you want
+#'
+#' @return A \code{data.frame} that works well with \code{kable}
+#' @export
+#'
+package_table = function(df) {
+  mine = df %>%
+    arrange(desc(open_issues_count), bare)
+  mine$issues_page = paste0("[", mine$open_issues_count, "](",
+                            mine$issues_page, ")")
+
+  mine = add_package_info(mine)
+  make_yes = function(x){
+    ifelse(x, '<i class="fa fa-check"></i>', '')
+  }
+  mine = mine %>%
+    mutate(
+      vignettes = make_yes(vignettes),
+      tests = make_yes(tests))
+
+  mine = mine %>%
+    select(gh_link, travis_badge,
+           appveyor_badge, issues_page, vignettes, tests,
+           version, cran_badge, dl_badge)
+  colnames(mine) = c("GitHub Link", "Travis Status",
+                     "Appveyor Status", "Number of Issues",
+                     "Vignettes Folder", "Tests Folder",
+                     "GH Version", "CRAN", "Downloads")
+  return(mine)
+}
